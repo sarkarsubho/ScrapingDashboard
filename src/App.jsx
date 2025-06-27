cd8785665ebce098ec3851688b759a531f87108c
sarkarsubho <sarkarsaby@gmail.com>
2025-06-27 23:19:38 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1 className="text-center">Scraping DashBoard</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
